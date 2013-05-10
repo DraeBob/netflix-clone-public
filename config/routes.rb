0cb8557a10f47a5be943d0d70af55eab3015ea86
Yuichi Hagio <yhagio87@gmail.com>
2013-05-10 10:57:05 -0400

    Added Register & Session routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@ Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
   root to: 'videos#index'
+  
   resources :videos, only: [:index, :show]
   resources :categories, except: [:edit, :update, :destroy]
+
   get '/search', to: 'videos#search'
+
+  resources :users, only: [:new, :create]
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end
