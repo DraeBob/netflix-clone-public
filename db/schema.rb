8e6b89aacac8fc914179bda437fe5cf52d610889
Yuichi Hagio <yhagio87@gmail.com>
2013-05-10 10:59:43 -0400

    Bootstrap_form, FactoryGirl installed, users controller & register page & database created

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130428180505) do
+ActiveRecord::Schema.define(:version => 20130510001144) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "fullname"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
   create_table "video_categories", :force => true do |t|
