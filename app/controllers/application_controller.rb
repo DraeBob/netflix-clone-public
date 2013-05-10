701781a25145d4ed8aebf92a432a9215414ffc56
Yuichi Hagio <yhagio87@gmail.com>
2013-05-10 19:33:27 -0400

    Register, login, logout with front page hooked up, not tested yet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :logged_in?, :current_user
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
   def require_user
     unless logged_in?
       flash[:error] = "You don't have permission to do this"
