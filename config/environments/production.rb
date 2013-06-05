175195d7cdf2db7d8328c0d9fda717c298c027c7
Yuichi Hagio <yhagio87@gmail.com>
2013-06-05 15:27:24 -0400

    production.rb modified

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,12 +65,12 @@ Myflix::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   #
-  
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => 'baci.lindsaar.net',
+    :domain               => 'http://myflix-yuichi.herokuapp.com',
     :user_name            => ENV["GMAIL_USERNAME"],
     :password             => ENV["GMAIL_PASSWORD"],
     :authentication       => 'plain',
