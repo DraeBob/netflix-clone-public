f05d3b761c4747844eba59e2dcbc8ef7d51c1e8a
Yuichi Hagio <yhagio87@gmail.com>
2013-06-05 13:09:14 -0400

    HW8-3

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -17,39 +17,60 @@ describe UsersController do
   end
 
   describe "POST create" do
-    let(:user) { Fabricate(:user)}
+    context "user registration process" do
+      let(:user) { Fabricate(:user)}
 
-    it "saves a new user in the database if the inputs are valid" do
-      expect {
+      it "saves a new user in the database if the inputs are valid" do
+        expect {
+          post :create, user: Fabricate.attributes_for(:user)
+        }.to change(User, :count).by(1)
+      end
+
+      it "redirect to videos_path if registered successfully" do
         post :create, user: Fabricate.attributes_for(:user)
-      }.to change(User, :count).by(1)
+        expect(response).to redirect_to videos_url
+      end
+
+      it "show flash message if registered successfully" do
+        post :create, user: Fabricate.attributes_for(:user)
+        expect(flash[:notice]).to eq("Successfully registered")
+      end
+
+      it "set current_user when registered successfully" do
+        post :create, user: Fabricate.attributes_for(:user)
+        expect(session[:user_id]).to eq(User.first.id)
+      end
+
+      it "Do not save the user if inputs are invalid" do
+        expect {
+          post :create, user: {fullname: nil}
+        }.to_not change(User, :count)    
+      end
+
+      it "render new page if inputs are invalid" do
+        post :create,
+          user: {fullname: nil}
+        expect(response).to render_template :new
+      end
     end
 
-    it "redirect to videos_path if registered successfully" do
-      post :create, user: Fabricate.attributes_for(:user)
-      expect(response).to redirect_to videos_url
-    end
+    context "email sending" do
+      it "sends out the email" do
+        post :create, user: Fabricate.attributes_for(:user)
+        expect(ActionMailer::Base.deliveries).not_to be_empty
+      end
 
-    it "show flash message if registered successfully" do
-      post :create, user: Fabricate.attributes_for(:user)
-      expect(flash[:notice]).to eq("Successfully registered")
-    end
+      it "sends out to the right recipient" do
+        post :create, user: Fabricate.attributes_for(:user)
+        message = ActionMailer::Base.deliveries.last
+        expect(message.to).to eq([User.last.email])
+      end
 
-    it "set current_user when registered successfully" do
-      post :create, user: Fabricate.attributes_for(:user)
-      expect(session[:user_id]).to eq(User.first.id)
-    end
-
-    it "Do not save the user if inputs are invalid" do
-      expect {
-        post :create, user: {fullname: nil}
-      }.to_not change(User, :count)    
-    end
-
-    it "render new page if inputs are invalid" do
-      post :create,
-        user: {fullname: nil}
-      expect(response).to render_template :new
+      it "has the right content" do
+        post :create, user: Fabricate.attributes_for(:user)
+        message = ActionMailer::Base.deliveries.last
+        expect(message.body).to include(User.last.fullname)
+      end
     end
   end
 end
