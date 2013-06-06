6a374a63c319879ef1364747e23b6ef4ac651f50
Yuichi Hagio <yhagio87@gmail.com>
2013-06-06 17:05:50 -0400

    7 routes for forget password

--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,11 @@ Myflix::Application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
+
+  get 'forgot_password', to: 'users/forgot_password'
+  put 'password_reset_token', to: 'users/password_reset_token'
+  get 'confirm_password_reset', to: 'users/confirm_password_reset'
+  get 'reset_password', to: 'users/reset_password'
+  put 'reset_password', to: 'users/reset_password'
+  get 'invalid_token', to: 'users/invalid_token'
 end
