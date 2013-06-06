c2658bda24c3f541d1f0c6d2b4cd8127f7780eb4
Yuichi Hagio <yhagio87@gmail.com>
2013-06-06 14:21:19 -0400

    default from, url setted

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,8 +1,8 @@
 class AppMailer < ActionMailer::Base
   default from: 'yuichih87@gmail.com'
-  
+
   def notify_on_new_user(user)
     @user = user
-    mail (to: user.email, subject: "You registered tealeaf myflix !")
+    mail to: user.email, subject: "You registered tealeaf myflix !"
   end
 end
