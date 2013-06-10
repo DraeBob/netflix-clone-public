8cf0ad20fcedaf548042740364b69bcdc68dd11c
Yuichi Hagio <yhagio87@gmail.com>
2013-06-09 20:10:20 -0400

    invitation form, controller, routes init

---
+++ app/controllers/invitations_controller.rb
@@ -0,0 +1,9 @@
+class InvitationsController < ApplicationController
+  before_filter :require_user
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -24,8 +24,11 @@ Myflix::Application.routes.draw do
 
   get '/forgot_password', to: 'forgot_passwords#new'
   resources :forgot_passwords, only: [:create]
-  get 'forgot_password_confirmation', to: 'forgot_passwords#confirm'
+  get '/forgot_password_confirmation', to: 'forgot_passwords#confirm'
 
   resources :password_resets, only: [:show, :create]
-  get 'expired_token', to: 'password_resets#expired_token'
+  get '/expired_token', to: 'password_resets#expired_token'
+
+  get '/invite', to: 'invitations#new'
+  resources :invitations, only: [:create]
 end
