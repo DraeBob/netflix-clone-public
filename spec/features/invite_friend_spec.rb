08609df445747f3858259e327eb2a6bc5c19ac01
Yuichi Hagio <yhagio87@gmail.com>
2013-06-14 13:17:36 -0400

    refactored features invite friend test

--- spec/features/invite_friend_spec.rb
+++ spec/features/invite_friend_spec.rb
@@ -5,35 +5,45 @@ feature 'Invite friend' do
     eminem = Fabricate(:user)
     sign_in(eminem)
 
-    # 1. Invite friend
+    invite_a_friend
+    friend_accepts_the_invitation
+
+    friend_follows_inviter(eminem)
+    inviter_follows_friend(eminem)
+
+    clear_email
+  end
+
+  def invite_a_friend
     visit invite_path
     fill_in "Friend's Name", with: 'Snoop Dogg'
     fill_in "Friend's Email Address", with: 'snoop@example.com'
     fill_in "Invitation Message", with: 'What up'
     click_button 'Send Invitation'
     click_link 'Sign Out'
+  end
 
-    # 2. Open email & click link
+  def friend_accepts_the_invitation
     open_email 'snoop@example.com'
     current_email.click_link 'Register at myflix from here'
 
-    # 3. Register information except email (prefilled)
     expect(page).to have_content 'Register'
     fill_in 'Password', with: 'password'
     fill_in 'Fullname', with: 'Snoop Dogg'
     click_button 'Register'
 
-    # 4. Signed in automatically and go to people page to check followership
     expect(page).to have_content("Successfully registered")
-    click_link 'People'
-    expect(page).to have_content(eminem.fullname)
-    click_link 'Sign Out'
+  end
 
-    # 5. Eminem checks the followership
-    sign_in(eminem)
+  def friend_follows_inviter(inviter)
+    click_link 'People'
+    expect(page).to have_content(inviter.fullname)
+    click_link 'Sign Out'
+  end
+
+  def inviter_follows_friend(inviter)
+    sign_in(inviter)
     click_link 'People'
     expect(page).to have_content('Snoop Dogg')
-
-    clear_email
   end
 end
