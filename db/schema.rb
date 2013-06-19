f6f984d466103ead5c03d9ac0e8753b5ea718209
Yuichi Hagio <yhagio87@gmail.com>
2013-06-19 06:30:15 -0400

    added admin column to users table and migrated/prepared

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130610180047) do
+ActiveRecord::Schema.define(:version => 20130619102821) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(:version => 20130610180047) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "token"
+    t.boolean  "admin"
   end
 
   create_table "video_categories", :force => true do |t|
