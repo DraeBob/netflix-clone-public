9775a930ecba4f26f6de0e80b7686e5eef0330ea
Yuichi Hagio <yhagio87@gmail.com>
2013-06-19 06:47:21 -0400

    created admins_controller.rb and require_admin method inside

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,7 @@
+class AdminsController < ApplicationController
+  before_filter :require_admin
+
+  def require_admin
+    flash[:error] = 'You do not have the permission to do this'
+    redirect_to root_path unless current_user.admin?
+end
